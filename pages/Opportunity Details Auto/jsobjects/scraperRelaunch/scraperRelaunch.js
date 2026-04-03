24f32002999233f545df4a4317668a290c8e52c8
quentin <quentin@ideel.io>
2026-04-03 14:23:33 +0000

    feat: target all scrapers in auto retry scraper

--- pages/Opportunity Details Auto/jsobjects/scraperRelaunch/scraperRelaunch.js
+++ pages/Opportunity Details Auto/jsobjects/scraperRelaunch/scraperRelaunch.js
@@ -1,11 +1,14 @@
 export default {
-	scraper: {
-		axa: false || checkbox_axa.isChecked,
-		ami_trois_f: false || checkbox_ami_trois_f.isChecked,
-		flitter: false || checkbox_flitter.isChecked,
-		novelia: false || checkbox_novelia.isChecked,
+	canRelaunch: JSObject1.valid(), // temp version (03.04.2026)
+	//canRelaunch: !(checkbox_ami_trois_f.isChecked || checkbox_axa.isChecked || checkbox_flitter.isChecked || checkbox_novelia.isChecked) || !JSObject1.valid(), // To use in Lancer button
+	scraper: { // On hold for now (03.04.2026)
+		//axa: false || checkbox_axa.isChecked,
+		//ami_trois_f: false || checkbox_ami_trois_f.isChecked,
+		//flitter: false || checkbox_flitter.isChecked,
+		//novelia: false || checkbox_novelia.isChecked,
 	},
-	 autoRefreshScraperResults () {
+	allScrapers: ["axa", "ami_trois_f", "flitter", "novelia", "april", "direct_assurance", "assurea"],
+	autoRefreshScraperResults () {
 		const intervalId = "scraperResultRefreshId";
 		const refreshRate = 60 * 1000; //in milliseconds
 
@@ -18,14 +21,15 @@ export default {
 				await getPotentialOffersOfLastRun.run();
 				await resetWidget("ScrapersCopy");
 				const chosenOffers = Quote.getChosenOffersRows();
-	      if (chosenOffers.length > 0) {
-			     await getProcedureById.run();
-			     AutoRefreshScraperResultSwitch.setValue(false)
-		     } 
+				if (chosenOffers.length > 0) {
+					await getProcedureById.run();
+					AutoRefreshScraperResultSwitch.setValue(false)
+				} 
 				await Garanties.run();
 
 			}, refreshRate ,intervalId);
 	},
+	// TODO: put back "targetPartnerLabels": scraperRelaunch.getRunningScraper(scraperRelaunch.scraper) in queries
 	getRunningScraper: (scraper) => {
 		const runningScraperList = Object.entries(scraper).filter(([, v]) => v === true)
 		const runningScraperLabel = runningScraperList.map(([k, ]) => k)
